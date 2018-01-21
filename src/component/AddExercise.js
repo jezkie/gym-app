f7d194943e682d6aa446811c072537a7e27ca538
Jescy Querimit <jezkie00secondary@gmail.com>
2018-01-21 17:49:03 +0800

    Created exercise form to be use in both adding and editing of exercise

--- src/component/AddExercise.js
+++ src/component/AddExercise.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormGroup, Col, ControlLabel/*, Form, Panel, Grid, Row*/ } from 'react-bootstrap';
-
-import { linkState, dropdownLinkState } from '../common/linkstate/LinkState';
-import TextInput from '../common/component/TextInput';
-import Dropdown from '../common/component/Dropdown';
 import { createExercise } from './ExerciseAction';
+import ExerciseForm from './ExerciseForm';
 import {
     CHEST_TYPE,
     SHOULDER_TYPE,
@@ -79,46 +75,7 @@ class AddExercise extends Component {
         console.log(this.props.match.params.param);
 
         return (
-            
-            <form onSubmit={this.save}>
-                <Col lg={2} lgOffset={4} md={4} mdOffset={4}>
-                    <FormGroup controlId='type'>
-                        <Col componentClass={ControlLabel}>
-                            Type
-                        </Col>
-                        <Col>
-                            <Dropdown items={this.state.types} 
-                                description={this.state.selectedTypeDescription} 
-                                valueLink={dropdownLinkState(this, 'type', 'selectedTypeDescription')} />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup controlId='name'>
-                        <Col componentClass={ControlLabel}>
-                            Name
-                        </Col>
-                        <Col>
-                            <TextInput valueLink={linkState(this, 'name')} placeholder='Exercise name' type='text'/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup controlId='sets'>
-                        <Col componentClass={ControlLabel}>
-                            Sets
-                        </Col>
-                        <Col>
-                            <TextInput valueLink={linkState(this, 'sets')} type='number'/>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup controlId='reps'>
-                        <Col componentClass={ControlLabel}>
-                            Reps
-                        </Col>
-                        <Col>
-                            <TextInput valueLink={linkState(this, 'reps')} type='number'/>
-                        </Col>
-                    </FormGroup>
-                    <input type="submit" value="Submit" />
-                </Col>
-            </form>
+            <ExerciseForm saveHandler={this.save} _this={this}/>
         );
     }
 }
