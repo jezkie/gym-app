bc691c53518479ec7b2eec0a0648b7887ca625d3
Jescy Querimit <jezkie00secondary@gmail.com>
2018-04-01 20:32:58 +0800

    Changed exercise list layout

--- src/component/exercise/ExerciseList.js
+++ src/component/exercise/ExerciseList.js
@@ -2,40 +2,53 @@ import React from 'react';
 import defaultImg from '../../images/default.svg';
 import addIcon from '../../images/Add-icon.png';
 import { LinkContainer } from 'react-router-bootstrap';
-
-import { Thumbnail, Button, Row, Grid, Col } from 'react-bootstrap';
+import { Item, Button } from 'semantic-ui-react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faPlus from '@fortawesome/fontawesome-free-solid/faPlus';
+import faEdit from '@fortawesome/fontawesome-free-solid/faEdit';
+import faTrash from '@fortawesome/fontawesome-free-solid/faTrash';
+import faPlay from '@fortawesome/fontawesome-free-solid/faPlay';
+import { Thumbnail, Row, Grid, Col } from 'react-bootstrap';
 
 const ExerciseList = ({ exercises, deleteHandler }) => {
     let exerciseList = exercises.map((item, i) => {
         return (
-            <Col xs={10} xsOffset={1} md={5} mdOffset={1} lg={3} key={i}>
-                <Thumbnail src={defaultImg}>
-                    <a onClick={() => deleteHandler(item.key)}>
-                        <span className="glyphicon glyphicon-remove"></span>
-                    </a>
-                    <h3>{item.name} {item.sets} x {item.reps}</h3>
-
-                    <LinkContainer to={`/log/start/${item.key}`}>
-                        <Button bsStyle="primary">Start</Button>
-                    </LinkContainer>&nbsp;
-                    <LinkContainer to={`/edit/exercise/${item.key}`}>
-                        <Button bsStyle="default">Edit</Button>
-                    </LinkContainer>
-                </Thumbnail>
-            </Col>
+            <Item key={i}>
+                <Item.Content>
+                    <Item.Header>
+                        {item.name}
+                    </Item.Header>
+                    <Item.Description>
+                        {item.sets} x {item.reps}
+                    </Item.Description>
+                    <Item.Extra>
+                        <LinkContainer to={`/log/start/${item.key}`} >
+                            <Button floated='left' primary circular><FontAwesomeIcon icon={faPlay} /></Button>
+                        </LinkContainer>
+                        <Button.Group floated='right'>
+                            <LinkContainer to={`/edit/exercise/${item.key}`}>
+                                <Button><FontAwesomeIcon icon={faEdit} /></Button>
+                            </LinkContainer>
+                            <Button.Or/>
+                            <Button onClick={() => deleteHandler(item.key)} floated='right' negative><FontAwesomeIcon icon={faTrash} /></Button>
+                        </Button.Group>
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
         )
     })
     return (
         <Grid>
-            <Row>
-                <Col xs={10} xsOffset={1} md={5} mdOffset={1} lg={3}>
-                    <LinkContainer to='/add/exercise'>
-                        <Thumbnail src={addIcon}>
-                        </Thumbnail>
-                    </LinkContainer>
-                </Col>
+            <Item.Group divided>
+                <Item>
+                    <Item.Content>
+                        <LinkContainer to='/add/exercise'>
+                            <Button positive><FontAwesomeIcon icon={faPlus} /></Button>
+                        </LinkContainer>
+                    </Item.Content>
+                </Item>
                 {exerciseList}
-            </Row>
+            </Item.Group>
         </Grid>
     )
 };
